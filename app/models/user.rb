42c8bf10366324cdeb04e7a363e174f9e0702640
che <che.blancoo58@gmail.com>
2020-12-04 22:30:13 +0100

    modify database

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
-    has_many :posts
+    has_many :posts dependent::destroy
+    has_many :comments dependent::destroy
     validates :username, presence:true, uniqueness:true
     validates :email, presence:true, uniqueness:true
     validates :password, presence:true, uniqueness:true,length: { in: 8..20 }
--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_04_201455) do
+ActiveRecord::Schema.define(version: 2020_12_04_203500) do
 
-  create_table "posts", force: :cascade do |t|
-    t.integer "user_id"
-    t.string "title"
-    t.string "description"
+  create_table "comments", force: :cascade do |t|
+    t.text "comment_body"
+    t.integer "user_id", null: false
+    t.integer "post_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.string "description"
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -29,4 +40,7 @@ ActiveRecord::Schema.define(version: 2020_12_04_201455) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "comments", "posts"
+  add_foreign_key "comments", "users"
+  add_foreign_key "posts", "users"
 end
