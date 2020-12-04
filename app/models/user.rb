0efe650e5b09c4f4636043e3cdefd86c2e52c40b
che <che.blancoo58@gmail.com>
2020-12-04 21:32:54 +0100

    Add playing with associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+    has_many :posts
     validates :username, presence:true, uniqueness:true
     validates :email, presence:true, uniqueness:true
     validates :password, presence:true, uniqueness:true,length: { in: 8..20 }
