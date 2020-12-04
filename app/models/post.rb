715cae9e1a7a4798ea43dfe3a72e5d99dc2528ef
Marcos Hernández Campos <marcoshdezcam@gmail.com>
2020-12-04 16:12:00 -0600

    Install Rubocop and configure GitHub Workflows

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
-    belongs_to :user
-    has_many   :comments ,dependent: :destroy
-    validates :description, presence:true
-    validates :title, uniqueness:true
+  belongs_to :user
+  has_many :comments, dependent: :destroy
+  validates :description, presence: true
+  validates :title, uniqueness: true
 end
